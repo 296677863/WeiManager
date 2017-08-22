db5ac4aa83e1614afacab2cdf69c32125e81ad6d
zfl <zfl@192.168.1.107>
2017-08-22 15:23:54 +0800

    字典管理

---
+++ WeiWeb/src/main/java/com/weiweb/system/controller/SysDictController.java
@@ -0,0 +1,7 @@
+package com.weiweb.system.controller;
+
+import com.weiweb.common.controller.BaseController;
+
+public class SysDictController  extends BaseController {
+
+}
---
+++ WeiWeb/src/main/java/com/weiweb/system/service/impl/SysDictServiceImpl.java
@@ -0,0 +1,7 @@
+package com.weiweb.system.service.impl;
+
+import com.weiweb.system.service.SysDictService;
+
+public class SysDictServiceImpl implements SysDictService{
+
+}
