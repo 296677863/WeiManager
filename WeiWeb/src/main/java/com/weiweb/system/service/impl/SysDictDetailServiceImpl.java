0bad1805cce71c4d06617bfe3bed6bd5db4412fb
zfl <zfl@192.168.1.109>
2017-08-25 09:38:27 +0800

    字典管理

---
+++ WeiWeb/src/main/java/com/weiweb/system/service/impl/SysDictDetailServiceImpl.java
@@ -0,0 +1,13 @@
+package com.weiweb.system.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.weiweb.core.mybatis.BaseMybatisDao;
+import com.weiweb.system.dao.SysDictDetailMapper;
+import com.weiweb.system.service.SysDictDetailService;
+
+@Service
+public class SysDictDetailServiceImpl extends BaseMybatisDao<SysDictDetailMapper>  implements SysDictDetailService{
+	
+
+}
